1784a6fd859514e3668b6f416b8feb771f10325e
dfbng353 <bousselmi.chema@gmail.com>
2026-08-05 16:15:31 +0100

    removed stupid swap files ugh

--- include/bus/bus.cpp
+++ include/bus/bus.cpp
@@ -1,8 +1,10 @@
 #include "../cpu/cpu.h"
 #include "bus.h"
-void write(uint16_t address, cpu_t *cpu){
-	cpu->data_bus=mem[address];
+void write(uint16_t address,cpu_t*cpu){
+	
 }
 uint8_t read(uint16_t address){
-	return mem[address];
+	if (address<0x4000 || >0x401000){
+		memory::read
+	}
 }
