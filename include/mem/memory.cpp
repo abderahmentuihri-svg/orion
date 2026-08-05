d2f0d23ef2d6921ca0d1b11c1806c95c9d1e7dfe
dfbng353 <bousselmi.chema@gmail.com>
2026-08-05 17:35:28 +0100

    modified gitignore to ignore object files i think segmentation fault error when out of bounds memory is accessed or you try to write to rom

--- include/mem/memory.cpp
+++ include/mem/memory.cpp
@@ -1,17 +1,16 @@
 #include "memory.h"
 uint8_t memory_t::read(uint32_t address){
-	if (address<=0x401000){
+	if (address<0x401000){
 		return ram[address];
 	}
 	else if (address<=0x409000){
 		return rom[address-0x401000];
 	}
-	else if (address>409000){
-		assert(false && "out-of-bounds mem read");
-	}
+	else{assert(false && "segmentation fault");return 1;}
 }
 void memory_t::write(uint32_t address,uint8_t value){
-	if (address<=0x401000){
+	if (address<0x401000){
 		ram[address]=value;
 	}
+	else{assert(false && "segmentation fault");
 }
